9534de2e6e5d66c3793804da85ae1bf257908270
leonardo300250 <leonardo30andradeazevedo@gmail.com>
2025-10-15 21:34:42 -0300

    correção

--- numero_secreto.py
+++ numero_secreto.py
@@ -3,7 +3,7 @@ print("Seja Bem Vindo Jogador(a): {}".format(nome))
 
 respCerta = 10
 
-tentativa = int(input('Tente advinhar o Número certo: '))
+tentativa = int(input('Tente advinhar o Número certo (APENAS UMA CHANCE): '))
 if tentativa<respCerta:
     print('Você errou o Número e MAIOR, Tente Novamente')
 elif tentativa>respCerta:
