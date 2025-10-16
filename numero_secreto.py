105a3a71df5d17888409cb84685b71bcd4408fc3
leonardo300250 <leonardo30andradeazevedo@gmail.com>
2025-10-15 21:32:14 -0300

    coreção de texto

--- numero_secreto.py
+++ numero_secreto.py
@@ -5,8 +5,8 @@ respCerta = 10
 
 tentativa = int(input('Tente advinhar o Número certo: '))
 if tentativa<respCerta:
-    print('Você errou o Número e maior, Tente Novamente')
+    print('Você errou o Número e MAIOR, Tente Novamente')
 elif tentativa>respCerta:
-    print('Você errou o Número é menor, Tente Novamente')
+    print('Você errou o Número é MENOR, Tente Novamente')
 else:
     print('Parabéns Você Acertou!!!')
